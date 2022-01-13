9ccdbe424b6762d4128c51789f2095cd9fbba2db
Henry <hhpendleton@gmail.com>
2022-01-13 11:26:49 -0500

    Dynamic Routing

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Date from "../components/date";
 import styles from "../styles/Home.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+export default function Home({ allPostsData }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -68,6 +79,22 @@ export default function Home() {
           </span>
         </a>
       </footer>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
